06a9dec3dcd1a9bad2d9e80a70fae891764db897
saranyasidharth <saranyasidhartht@gmail.com>
2022-10-15 16:05:29 +0530

    second

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signin',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+  loginForm!: FormGroup
+
+  constructor(private formbuilder: FormBuilder, private http: HttpClient, private route: Router) {
+
+  }
 
   ngOnInit(): void {
+    this.loginForm = this.formbuilder.group({
+      email: [''],
+      pwd: ['']
+    })
+
+  }
+  login() {
+    this.http.get<any>('http://localhost:3000/user').
+      subscribe(res => {
+
+        const user = res.find((a: any) => {
+          return a.email === this.loginForm.value.email && a.pwd === this.loginForm.value.pwd;
+        }
+
+
+        )
+        if (user) {
+          alert("login success");
+          this.loginForm.reset;
+
+        } else {
+          alert("user does not exist");
+
+        }
+      })
   }
 
 }
